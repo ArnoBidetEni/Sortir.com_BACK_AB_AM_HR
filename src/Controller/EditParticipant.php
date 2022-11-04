8fea1716080d70a5bc32e342282a07eda2403488
ArthurMannevilleENI <arthur.manneville2021@campus-eni.fr>
2022-11-04 16:57:25 +0100

    [EDIT] Suppression de code commenté

--- src/Controller/EditParticipant.php
+++ src/Controller/EditParticipant.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 #[AsController]
@@ -20,42 +18,6 @@ class EditParticipant extends AbstractController
         private ManagerRegistry $doctrine,
         private UserPasswordHasherInterface $passwordHasher
     ){}
-
-    // #[Route('/api/participants/{participantId}', name: 'api_edit_participant')]
-    // public function editParticipant(Request $request, int $participantId): JsonResponse
-    // {
-    //     $participant = $this->doctrine->getRepository(Participant::class)->findOneBy(['participantId' => $participantId]);
-
-    //     if ($participant)
-    //     {
-    //         $requestDatas = json_decode($request->getContent());
-
-    //         $participant->setLastName($requestDatas->lastName);
-    //         $participant->setFirstName($requestDatas->firstName);
-    //         $participant->setLogin($requestDatas->login);
-    //         $participant->setPhoneNumber($requestDatas->phoneNumber);
-    //         $participant->setMail($requestDatas->mail);
-
-    //         if ($requestDatas->password) {
-    //             $hashedPassword = $this->passwordHasher->hashPassword(
-    //                 $participant,
-    //                 $requestDatas->password
-    //             );
-
-    //             $participant->setPassword($hashedPassword);
-    //         }
-
-    //         $em = $this->doctrine->getManager();
-    //         $em->persist($participant);
-    //         $em->flush();
-
-    //     } else {
-
-    //         throw new NotFoundResourceException("No Participant found for this identifier");
-    //     }
-
-    //     return new JsonResponse("Participant well edited");
-    // }
 
     public function __invoke(Participant $participant, Request $request): JsonResponse
     {
